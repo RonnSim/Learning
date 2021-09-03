f2f859973e62f0238f952d3874516e33edb28046
Sim Yuan Siang <75885632+RonnSim@users.noreply.github.com>
2021-09-03 15:33:39 +0800

    1

--- Java.java
+++ Java.java
@@ -1,1 +1,6 @@
-
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("This statement creates a new line after this output")
+    System.out.print("This statement outputs will be in the same line as the next")
+  }
+}
