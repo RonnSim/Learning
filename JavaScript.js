22d6f6761b9bb1eeab01b31d1e84d3fee11a1906
Ronn <75885632+RonnSim@users.noreply.github.com>
2021-09-19 00:26:56 +0800

    Intro

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,30 @@
+// Introduction:     
+      // Console - keyword referring to an object/collection of data and actions
+          // .log() method - console.log() prints/logs to console whatever is inside "()"
+          console.log(5);
+          // output: 5
 
+      // Datatypes:
+          /* Number
+              String
+              Boolean
+              null - intentional absence of value
+              undefined
+              Symbol - unique identifiers for complex coding
+              Object - collections of related data */
+
+      // Arithmetic Operators
+          // +, -, *, /, %
+          console.log(3 + 4);
+          console.log(5 - 1); 
+          console.log(4 * 2); 
+          console.log(9 / 3);
+          console.log(11 % 3);
+
+      // String concatenation
+          console.log('hi' + 'ya'); // Prints 'hiya'
+          console.log('wo' + 'ah'); // Prints 'woah'
+          console.log('front ' + 'space'); // Prints 'front space'
+          console.log('back' + ' space'); // Prints 'back space'
+          console.log('no' + 'space'); // Prints 'nospace'
+          console.log('middle' + ' ' + 'space'); // Prints 'middle space'
